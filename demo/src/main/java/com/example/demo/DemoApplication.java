b943dcb1268270692697f30ca6c7d6f9f04ed1de
MewPK <milzaza123@gmail.com>
2022-12-26 23:37:23 +0700

    Successful Create API With Class Student ConStructor

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,8 +18,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping("/")
-	public List<String> hello(){
-		return List.of("Hello World");
+	public List<Student> hello(){
+		return List.of(new Student(
+				1L, "MewPK",
+				"mew@gmail.com",
+				20,
+				LocalDate.of(2000, Month.FEBRUARY,25)
+
+		));
 	}
 
 }
